596c91c0e00e81e770b71b6c2a7d37aea0f04c5b
SAlmendro <99258391+SAlmendro@users.noreply.github.com>
2022-03-14 20:55:43 +0100

    Created iOS app project

---
+++ iOSApp/Fuel Compense/Fuel Compense/Fuel_CompenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fuel_CompenseApp.swift
+//  Fuel Compense
+//
+//  Created by Sergio Almendro Cerdá on 14/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Fuel_CompenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSApp/Fuel Compense/Fuel CompenseUITests/Fuel_CompenseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fuel_CompenseUITestsLaunchTests.swift
+//  Fuel CompenseUITests
+//
+//  Created by Sergio Almendro Cerdá on 14/3/22.
+//
+
+import XCTest
+
+class Fuel_CompenseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
