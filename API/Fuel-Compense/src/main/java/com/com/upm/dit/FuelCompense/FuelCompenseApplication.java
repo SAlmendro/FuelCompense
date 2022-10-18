9a1f5becf6ae14bd882dac8bd7c51fddff5dabba
SAlmendro <salmendro97@gmail.com>
2022-10-18 20:50:05 +0200

    Created structure for SpringBoot API

---
+++ API/Fuel-Compense/src/main/java/com/com/upm/dit/FuelCompense/FuelCompenseApplication.java
@@ -0,0 +1,13 @@
+package com.com.upm.dit.FuelCompense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelCompenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelCompenseApplication.class, args);
+	}
+
+}
---
+++ API/Fuel-Compense/src/test/java/com/com/upm/dit/FuelCompense/FuelCompenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.upm.dit.FuelCompense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelCompenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
