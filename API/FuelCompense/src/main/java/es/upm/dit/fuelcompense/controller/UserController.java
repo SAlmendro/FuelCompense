4188857fc22f92d079f9fa8067026ece0e250090
SAlmendro <salmendro97@gmail.com>
2023-09-06 16:35:30 +0200

    Added logs to control calls to API from console better

--- API/FuelCompense/src/main/java/es/upm/dit/fuelcompense/controller/UserController.java
+++ API/FuelCompense/src/main/java/es/upm/dit/fuelcompense/controller/UserController.java
@@ -7,6 +7,8 @@ import es.upm.dit.fuelcompense.service.dto.UserDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/users")
@@ -22,36 +24,43 @@ public class UserController {
 
     @PostMapping(value = "/new")
     public User createUser(@RequestBody UserDTO userDTO) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha llamado al endpoint de crear usuario con el username: " + userDTO.getUserName());
         return this.userService.createUser(userDTO);
     }
 
     @PostMapping(value = "/follow/{followedUserName}")
     public List<String> follow(@RequestBody String followerUserName, @PathVariable("followedUserName") String followedUserName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha llamado al endpoint de seguir a un usuario. Seguidor: " + followerUserName + " Seguido: " + followedUserName);
         return this.userService.follow(followerUserName, followedUserName);
     }
 
     @GetMapping(value = "/following/{followerUserName}")
     public List<String> findAllFollowing(@PathVariable("followerUserName") String followerUserName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha solicitado la lista de seguidos para el usuario " + followerUserName);
         return this.userService.findAllFollowing(followerUserName);
     }
 
     @GetMapping(value = "/followers/{followedUserName}")
     public List<String> findAllFollowers(@PathVariable("followedUserName") String followedUserName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha solicitado la lista de seguidores para el usuario " + followedUserName);
         return this.userService.findAllFollowers(followedUserName);
     }
 
     @GetMapping
     public List<UserDTO> findAll() {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha solicitado la lista completa de usuarios");
         return userMapperOut.listMap(this.userService.findAll());
     }
 
     @GetMapping(value = "/{userName}")
     public UserDTO findUser(@PathVariable("userName") String userName){
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha solicitado el login del usuario " + userName);
         return userMapperOut.map(this.userService.findUserByUserName(userName));
     }
 
     @DeleteMapping("/{userName}")
     public Boolean deleteUser(@PathVariable String userName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se ha solicitado eliminar el usuario " + userName);
         Long userId = userService.findUserByUserName(userName).getId();
         return userService.deleteUserById(userId);
     }
