ea01d98ad301986130dfd65255b2327a3d8d238a
SAlmendro <salmendro97@gmail.com>
2023-10-15 19:21:26 +0200

    Added logs to endpoints

--- API/FuelCompense/src/main/java/es/upm/dit/fuelcompense/controller/StatusController.java
+++ API/FuelCompense/src/main/java/es/upm/dit/fuelcompense/controller/StatusController.java
@@ -7,6 +7,8 @@ import es.upm.dit.fuelcompense.service.dto.StatusDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/statuses")
@@ -24,36 +26,43 @@ public class StatusController {
 
     @PostMapping(value = "/new")
     public StatusDTO createStatus(@RequestBody StatusDTO statusDTO) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se esta intentando crear un estado. Usuario " + statusDTO.getAuthUserName());
         return this.statusMapperOut.map(this.statusService.createStatus(statusDTO));
     }
 
     @GetMapping
     public List<StatusDTO> findAll() {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se han buscado todos los estados");
         return this.statusMapperOut.listMap(this.statusService.findAll());
     }
 
     @GetMapping(value = "/{userName}")
     public List<StatusDTO> findAllByUserName(@PathVariable("userName") String userName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se han buscado todos los estados de " + userName);
         return this.statusMapperOut.listMap(this.statusService.findAllStatusesByCreatorId(this.userService.findUserByUserName(userName).getId()));
     }
 
     @GetMapping(value = "/subscribed/{userName}")
     public List<StatusDTO> findAllBySubscriberUserName(@PathVariable("userName") String userName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se han buscado todos los estados a los que esta suscrito " + userName);
         return this.statusMapperOut.listMap(this.statusService.findAllStatusesBySubscriberUserName(userName));
     }
 
     @DeleteMapping(value = "/{id}")
     public Boolean deleteStatus(@PathVariable("id") Long id) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se intenta borrar el estado " + id);
         try {
             this.statusService.deleteStatus(id);
             return true;
         } catch (Exception e) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, "Ha habido un error borrando el estado. Traza: " + e.getMessage());
             return false;
         }
     }
 
     @PostMapping(value = "/fav/{statusId}")
     public Boolean updateFav(@PathVariable("statusId") Long statusId, @RequestBody String userName) {
+        Logger.getAnonymousLogger().log(Level.WARNING, "Se han buscado todos los estados favs de " + userName);
         return this.statusService.changeFavorite(statusId, userName).get();
     }
 
