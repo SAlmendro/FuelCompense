20598fdf670746e19b2f9c368cc58df6c1648e81
SAlmendro <salmendro97@gmail.com>
2023-10-15 19:31:17 +0200

    Created refresh button to retrieve the statuses

---
+++ iOSApp/Fuel Compense/Fuel Compense/Elements/RefreshButton.swift
@@ -0,0 +1,30 @@
+//
+//  RefreshButton.swift
+//  Fuel Compense
+//
+//  Created by Sergio Almendro Cerdá on 14/9/23.
+//
+
+import SwiftUI
+
+struct RefreshButton: View {
+    @EnvironmentObject var statusModel: StatusModel
+    @State var title : String
+    
+    var body: some View {
+        HStack{
+            Spacer()
+            Text(title)
+            Spacer()
+            Button(action: {
+                statusModel.getSubscribedStatuses()
+            }) {
+                Image(systemName: "goforward")
+                    .resizable()
+            }
+            .frame(width: 25, height: 25)
+            .padding()
+        }
+        
+    }
+}
